94201cc6e083bbe6d65a4c59776659f29903e89d
wayo848yowa <142649823+wayo848yowa@users.noreply.github.com>
2024-11-30 04:48:17 +0000

    ガイド14 終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  root "hello#index"
+  get 'pages/about'
+  resources :ideas do
+    resources :comments
+  end
+  root "pages#homepage"
 end
---
+++ spec/models/idea_spec.rb
@@ -0,0 +1,9 @@
+require "rails_helper"
+
+RSpec.describe Idea do
+  it "has a name" do # yep, you can totally use 'it'
+    idea = Idea.create!(name: "My Awesome Idea Name") # creating a new idea 'instance'
+    second_idea = Idea.create!(name: "My Second Idea Name") # creating another new idea 'instance'
+    expect(second_idea.name).to eq("My Second Idea Name") # this is our expectation
+  end
+end
